30e267243bdb6458b0fd4615c0686c9180032370
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-08-18 16:02:41 +0530

    🚧Used video-react :)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+// import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 import "./app.scss";
+import "/node_modules/video-react/dist/video-react.css";
+import { Player } from "video-react";
 
 function App() {
   return (
     <>
-      <VideoPlayer />
+      <Player
+        playsInline
+        poster="/assets/poster.png"
+        src="http://localhost:8000/video/vampire"
+      />
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -35,6 +35,33 @@ app.get("/video", (req, res) => {
   const videoStream = fs.createReadStream(videoPath, { start, end });
   videoStream.pipe(res);
 });
+app.get("/video/:id", (req, res) => {
+  const videoId = req.params.id;
+  // Your existing video streaming code remains the same
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send("Requires RAnge header");
+  }
+
+  const videoPath = `${videoId}.mp4`;
+  const videoSize = fs.statSync(`${videoId}.mp4`).size;
+  const CHUNK_SIZE = 10 ** 6; //1MB
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start} - ${end}/${videoSize}`,
+    "Accept-Range": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  res.writeHead(206, headers);
+
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+  videoStream.pipe(res);
+});
 
 app.listen(8000, () => {
   console.log("Listening on port 8000!");
